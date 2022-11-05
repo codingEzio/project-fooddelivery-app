7ecc5275bf05bc2e724315cc13f5fea7f5beeba2
!Mensch <assassinste@gmail.com>
2022-11-06 00:23:51 +0800

    feat: Implement a Header component and import as Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Home } from './components';
 
 const App = () => {
   return (
-      <div className="w-screen h-screen flex items-center justify-center text-cyan-400">
-        Worlds 2022
-      </div>
+    <div className="w-screen h-auto flex flex-col">
+      <Home />
+    </div>
   );
 };
 
