4be211f072fd5e49c3c58ffe586621fc67780a25
!Mensch <assassinste@gmail.com>
2022-11-06 00:31:51 +0800

    feat: Responsive header (added color for distinction)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+      {/* Desktop and Tablet */}
+      <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+
+      {/* Mobile */}
+      <div className="flex md:hidden w-full h-full bg-blue-400 p-4"></div>
+    </div>
+  );
 };
 
 export default Header;
