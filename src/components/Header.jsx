f7559d63bd86095f3618b6459612c1cb3476df39
!Mensch <assassinste@gmail.com>
2022-11-06 00:37:26 +0800

    dev: Remove code for testing (changing background color)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
       {/* Desktop and Tablet */}
-      <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+      <div className="hidden md:flex w-full p-4"></div>
 
       {/* Mobile */}
-      <div className="flex md:hidden w-full h-full bg-blue-400 p-4"></div>
+      <div className="flex md:hidden w-full h-full p-4"></div>
     </div>
   );
 };
