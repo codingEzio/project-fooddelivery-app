227c081b5fdb4fd8e25045cf48340d06fb7f68cd
!Mensch <assassinste@gmail.com>
2022-11-06 00:54:35 +0800

    feat: Logo and navbar for the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import Logo from '../img/logo.png';
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <header className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
       {/* Desktop and Tablet */}
-      <div className="hidden md:flex w-full p-4"></div>
+      <div className="hidden md:flex w-full h-full">
+        <div className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="An logo" />
+
+          <p className="text-headingColor text-xl font-bold">City Food</p>
+        </div>
+
+        <ul className="flex items-center gap-8 ml-auto">
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Menu
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            About Us
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Services
+          </li>
+        </ul>
+      </div>
 
       {/* Mobile */}
       <div className="flex md:hidden w-full h-full p-4"></div>
-    </div>
+    </header>
   );
 };
 
