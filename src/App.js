436c11a6e7655fb2ac46ad27b8e861ceca54190b
!Mensch <assassinste@gmail.com>
2022-11-06 23:13:22 +0800

    fix: Change component name for exporting

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
-import { Home, MainContainer, CreateContainer } from './components';
+import { Header, MainContainer, CreateContainer } from './components';
 
 const App = () => {
   return (
     <AnimatePresence mode="wait">
       <div className="w-screen h-auto flex flex-col bg-primary">
-        <Home />
+        <Header />
 
         {/* note: Move the title closer to header on mobile */}
         <main className="mt-16 md:mt-24 p-8 w-full">
