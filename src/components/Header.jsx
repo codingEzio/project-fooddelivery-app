1ad97a5809aa8bbd4919db7de28f2deb7063d923
!Mensch <assassinste@gmail.com>
2022-11-06 12:08:18 +0800

    feat: Integrate Firebase authenticate with Google

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { MdShoppingBasket } from 'react-icons/md';
 import { motion } from 'framer-motion';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { app } from '../firebase.config';
 
 import Logo from '../img/logo.png';
 import Avatar from '../img/avatar.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const login = () => {
-    alert(1000);
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const login = async () => {
+    const response = await signInWithPopup(firebaseAuth, provider);
+
+    console.log(response);
   };
 
   return (
