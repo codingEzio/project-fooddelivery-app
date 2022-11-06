25448b8aa79ced184676a6d0421e71a3aaa15d65
!Mensch <assassinste@gmail.com>
2022-11-06 18:19:48 +0800

    feat: Mobile layout -> dropdown for add item and log out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -138,6 +138,42 @@ const Header = () => {
           <img src={Logo} className="w-8 object-cover" alt="An logo" />
           <p className="text-headingColor text-xl font-bold">City Food</p>
         </Link>
+
+        <div className="relative">
+          {/* Logo */}
+          <motion.img
+            whileTap={{ scale: 0.7 }}
+            src={user ? user.photoURL : Avatar}
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-lg"
+            alt="User profile avatar"
+            onClick={login}
+          />
+
+          {/* Navbar (all combined in one dropdown list) */}
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+            >
+              {user && user.email === process.env.REACT_APP_ADMIN_EMAIL_ADDR && (
+                <Link to={'/createItem'}>
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    <MdAdd /> New Item
+                  </p>
+                </Link>
+              )}
+
+              <p
+                className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base"
+                onClick={logout}
+              >
+                <MdLogout /> Logout
+              </p>
+            </motion.div>
+          )}
+        </div>
       </div>
     </header>
   );
