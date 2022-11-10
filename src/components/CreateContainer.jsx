2361606b6581e0844cbae727414d7a65bb5fc51b
!Mensch <assassinste@gmail.com>
2022-11-10 13:54:17 +0800

    dev: Initialize the states we need and basic page layout

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const CreateContainer = () => {
+  const [title, setTitle] = useState("");
+  const [calories, setCalories] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState(null);
+
+  const [fields, setFields] = useState(true);
+  const [alertStatus, setAlertStatus] = useState("danger");
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div>
-      <h1>CreateContainer</h1>
+    <div className="flex w-full min-h-screen items-center justify-center">
+      <div className="flex flex-col w-[90%] md:w-[75%] border-gray-300 rounded-lg p-4 items-center justify-center">
+        1
+      </div>
     </div>
   );
 };
