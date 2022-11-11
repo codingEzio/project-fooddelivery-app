b8cc2e2ea8862c7b298f67437c7982dfeb1d25ad
!Mensch <assassinste@gmail.com>
2022-11-11 11:52:39 +0800

    feat: Able to click through each categories with styling changes

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoFastFood } from 'react-icons/io5';
 
 import { categories } from '../utils/data';
 
 const MenuContainer = () => {
   const [filter, setFilter] = useState('chicken');
+
+  useEffect(() => {}, [filter]);
 
   return (
     <section className="w-full my-6" id="menu">
@@ -21,13 +23,36 @@ const MenuContainer = () => {
             categories.map(sole_category => (
               <div
                 key={sole_category.id}
-                className="flex flex-col w-24 min-w-[94px] h-28 bg-card hover:bg-cartNumBg group cursor-pointer rounded-lg drop-shadow-xl gap-3 transition-all ease-in-out duration-150 items-center justify-center"
+                className={`flex flex-col w-24 min-w-[94px] h-28 ${
+                  filter === sole_category.urlParamName
+                    ? 'bg-cartNumBg'
+                    : 'bg-card'
+                } hover:bg-cartNumBg group cursor-pointer rounded-lg drop-shadow-xl gap-3 transition-all ease-in-out duration-150 items-center justify-center`}
+                onClick={() => setFilter(sole_category.urlParamName)}
               >
-                <div className="flex w-10 h-10 bg-cartNumBg group-hover:bg-card rounded-full items-center justify-center">
-                  <IoFastFood className="text-card text-lg group-hover:text-textColor" />
+                <div
+                  className={`flex w-10 h-10 shadow-lg ${
+                    filter === sole_category.urlParamName
+                      ? 'bg-white'
+                      : 'bg-cartNumBg'
+                  } group-hover:bg-white rounded-full items-center justify-center`}
+                >
+                  <IoFastFood
+                    className={`${
+                      filter === sole_category.urlParamName
+                        ? 'text-textColor'
+                        : 'text-white'
+                    } text-lg group-hover:text-textColor`}
+                  />
                 </div>
 
-                <p className="text-sm text-textColor group-hover:text-white">
+                <p
+                  className={`text-sm ${
+                    filter === sole_category.urlParamName
+                      ? 'text-white'
+                      : 'text-textColor'
+                  } group-hover:text-white`}
+                >
                   {sole_category.name}
                 </p>
               </div>
