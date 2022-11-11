0b34c327b443f5277299303d37d8356e7ec75bb6
!Mensch <assassinste@gmail.com>
2022-11-11 11:24:04 +0800

    dev: Category placeholder for filtering (no functionality yet)

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoFastFood } from 'react-icons/io5';
 
 const MenuContainer = () => {
   return (
@@ -10,6 +11,18 @@ const MenuContainer = () => {
         >
           Our hot dishes
         </p>
+
+        <div className="flex w-full lg:justify-center items-center justify-center gap-8 py-6 overflow-x-scroll scrollbar-none">
+          <div className="flex flex-col w-24 min-w-[94px] h-28 bg-card hover:bg-cartNumBg group cursor-pointer rounded-lg drop-shadow-xl gap-3 transition-all ease-in-out duration-150 items-center justify-center">
+            <div className="flex w-10 h-10 bg-cartNumBg group-hover:bg-card rounded-full items-center justify-center">
+              <IoFastFood className="text-card text-lg group-hover:text-textColor" />
+            </div>
+
+            <p className="text-sm text-textColor group-hover:text-white">
+              Category
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
