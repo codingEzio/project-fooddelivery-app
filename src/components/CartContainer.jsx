c0214b82040f10c2aff703ece1e9003d9564ad0c
!Mensch <assassinste@gmail.com>
2022-11-11 23:00:44 +0800

    fix: Correct closing div tag for card items and buttons

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -81,43 +81,43 @@ const CartContainer = () => {
                   </div>
                 </div>
               ))}
+          </div>
 
-            {/* Cart -> Total Summary */}
-            <div className="flex flex-col flex-1 w-full px-8 py-2 bg-cartTotal rounded-t-[2rem] items-center justify-evenly">
-              <div className="flex w-full items-center justify-between">
-                <p className="text-gray-400 text-lg">Sub Total</p>
-                <p className="text-gray-400 text-lg">$ 8.5</p>
-              </div>
-              <div className="flex w-full items-center justify-between">
-                <p className="text-gray-400 text-lg">Delivery</p>
-                <p className="text-gray-400 text-lg">$ 3.0</p>
-              </div>
-
-              <div className="w-full my-2 border-b border-gray-600"></div>
-
-              <div className="flex w-full items-center justify-between">
-                <p className="text-xl text-gray-200 font-semibold">Total</p>
-                <p className="text-xl text-gray-200 font-semibold">$11.5</p>
-              </div>
-
-              {user ? (
-                <motion.button
-                  whileTap={{ scale: 0.8 }}
-                  type="button"
-                  className="w-full text-lg p-2 my-2 text-gray-50 bg-gradient-to-tr from-orange-400 to-orange-600 rounded-full hover:shadow-lg"
-                >
-                  Check Out
-                </motion.button>
-              ) : (
-                <motion.button
-                  whileTap={{ scale: 0.8 }}
-                  type="button"
-                  className="w-full text-lg p-2 my-2 text-gray-50 bg-gradient-to-tr from-orange-400 to-orange-600 rounded-full italic hover:shadow-lg"
-                >
-                  Log in Required
-                </motion.button>
-              )}
+          {/* Cart -> Total Summary */}
+          <div className="flex flex-col flex-1 w-full px-8 py-2 bg-cartTotal rounded-t-[2rem] items-center justify-evenly">
+            <div className="flex w-full items-center justify-between">
+              <p className="text-gray-400 text-lg">Sub Total</p>
+              <p className="text-gray-400 text-lg">$ 8.5</p>
             </div>
+            <div className="flex w-full items-center justify-between">
+              <p className="text-gray-400 text-lg">Delivery</p>
+              <p className="text-gray-400 text-lg">$ 3.0</p>
+            </div>
+
+            <div className="w-full my-2 border-b border-gray-600"></div>
+
+            <div className="flex w-full items-center justify-between">
+              <p className="text-xl text-gray-200 font-semibold">Total</p>
+              <p className="text-xl text-gray-200 font-semibold">$11.5</p>
+            </div>
+
+            {user ? (
+              <motion.button
+                whileTap={{ scale: 0.8 }}
+                type="button"
+                className="w-full text-lg p-2 my-2 text-gray-50 bg-gradient-to-tr from-orange-400 to-orange-600 rounded-full hover:shadow-lg"
+              >
+                Check Out
+              </motion.button>
+            ) : (
+              <motion.button
+                whileTap={{ scale: 0.8 }}
+                type="button"
+                className="w-full text-lg p-2 my-2 text-gray-50 bg-gradient-to-tr from-orange-400 to-orange-600 rounded-full italic hover:shadow-lg"
+              >
+                Log in Required
+              </motion.button>
+            )}
           </div>
         </div>
       ) : (
