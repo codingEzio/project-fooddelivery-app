84bf107c6de51c026e472352042d706939cd55f1
!Mensch <assassinste@gmail.com>
2022-11-11 11:34:11 +0800

    dev: Loop through existing categories (filter)

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { IoFastFood } from 'react-icons/io5';
 
+import { categories } from '../utils/data';
+
 const MenuContainer = () => {
+  const [filter, setFilter] = useState('chicken');
+
   return (
     <section className="w-full my-6" id="menu">
       <div className="flex flex-col w-full items-center justify-center">
@@ -13,15 +17,21 @@ const MenuContainer = () => {
         </p>
 
         <div className="flex w-full lg:justify-center items-center justify-center gap-8 py-6 overflow-x-scroll scrollbar-none">
-          <div className="flex flex-col w-24 min-w-[94px] h-28 bg-card hover:bg-cartNumBg group cursor-pointer rounded-lg drop-shadow-xl gap-3 transition-all ease-in-out duration-150 items-center justify-center">
-            <div className="flex w-10 h-10 bg-cartNumBg group-hover:bg-card rounded-full items-center justify-center">
-              <IoFastFood className="text-card text-lg group-hover:text-textColor" />
-            </div>
+          {categories &&
+            categories.map(sole_category => (
+              <div
+                key={sole_category.id}
+                className="flex flex-col w-24 min-w-[94px] h-28 bg-card hover:bg-cartNumBg group cursor-pointer rounded-lg drop-shadow-xl gap-3 transition-all ease-in-out duration-150 items-center justify-center"
+              >
+                <div className="flex w-10 h-10 bg-cartNumBg group-hover:bg-card rounded-full items-center justify-center">
+                  <IoFastFood className="text-card text-lg group-hover:text-textColor" />
+                </div>
 
-            <p className="text-sm text-textColor group-hover:text-white">
-              Category
-            </p>
-          </div>
+                <p className="text-sm text-textColor group-hover:text-white">
+                  {sole_category.name}
+                </p>
+              </div>
+            ))}
         </div>
       </div>
     </section>
